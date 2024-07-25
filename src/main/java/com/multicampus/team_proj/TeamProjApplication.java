c9ed1d8c2d298a5fe323a519d47dff2e93ce1fdb
이석준 <tjrwns000617@naver.com>
2024-07-25 13:44:27 +0900

    초기세팅완료

---
+++ src/main/java/com/multicampus/team_proj/TeamProjApplication.java
@@ -0,0 +1,13 @@
+package com.multicampus.team_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/multicampus/team_proj/main/MainController.java
@@ -0,0 +1,14 @@
+package com.multicampus.team_proj.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+}
